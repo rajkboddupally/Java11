9329808e6cb9b8f569c3043087c042690821cd84
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-02-13 22:31:56 -0500

    Initial commit

---
+++ src/string/StringAPIEnhancements.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringAPIEnhancements {
+    public static void main(String[] args) {
+        String str = "hello ";
+
+        System.out.println(str.repeat(5));
+
+    }
+}
